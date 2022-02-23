6e9636abb3abac0389e603cd3137442ce5016751
4400112799 <4400112799@BT144-PROG22>
2022-02-23 14:50:11 -0700

    started new class

---
+++ CKK.Logic/Models/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace CKK.Logic.Models
+{
+    class ShoppingCart
+    {
+        private Customer _Customer; //instance variable for _Customer
+        private ShoppingCartItem _Product1; //instance variable for _Product1
+        private ShoppingCartItem _Product2; //intsance variable for _Product2
+        private ShoppingCartItem _Product3; //instance variable for _Product3
+
+
+        public ShoppingCart(Customer cust)
+        {
+            _Customer = cust; 
+        }
+
+
+        public int GetCustomerId()
+        {
+            return _Customer.GetId();
+        }
+
+        public ShoppingCartItem AddProduct(int Product, quantity prod)
+        {
+
+        }
+
+
+
+
+    }
+}
